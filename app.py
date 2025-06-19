414eb4de03f36f449639dc47a9f9f52a64978970
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-19 21:49:54 +0530

    Added title screen and image

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("Planet_Story")
 
 @app.route("/")
-def main():
-    return "hello"
+def title_screen():
+    return render_template("title.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
