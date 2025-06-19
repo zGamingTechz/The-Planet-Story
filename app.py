9c8693aec9f8ccfe6ab6d7bd97f2b80b45c257a8
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-19 22:09:14 +0530

    Added redirect to home

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("Planet_Story")
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def title_screen():
+    if request.method == "POST":
+        return "Home"
     return render_template("title.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
